88466b329990fe1cdb1e4ceb448a83311d37b11b
Nicolas Medeiros <nicolsoliverg@gmail.com>
2024-08-31 21:47:02 -0300

    Saving state

--- Scenes/collectible.gd
+++
@@ -1,6 +0,0 @@
-extends StaticBody2D
-
-@export var value = 10
-
-func collect():
-	queue_free()
--- Scenes/level.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-@onready var ui = %ui
-
-func update_ui_score(value):
-	ui.update_score(value)
